4e6ff85092a3aa281f9c5db1edf87120a195a89d
MAUNDU <78978450+maunduwebmaster@users.noreply.github.com>
2022-04-22 17:51:49 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function initMap() {
+  const loc = { lat: -34.397, lng: 150.644 };
+  const map = new google.maps.Map(document.querySelector('.map')
+   ,{
+     zoom: 14,
+     center: loc
+   });
+   const marker = new google.maps.Marker({ position: loc, map: map});
+}
+
+$('#navbar a, .btn').on('click', function (event) {
+  if(this.hash !==''){
+    event.preventDefault();
+    const hash = this.hash;
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+
+});
